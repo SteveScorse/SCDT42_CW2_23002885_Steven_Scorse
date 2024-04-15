04ed5ea022c4d8a83fa8b9adfa4fe751435f6205
SteveScorse <scorsesteven@gmail.com>
2024-04-15 12:36:08 +0100

    Contact Page COMPLETE

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-a
+//Google Maps API 
+let map;
+
+async function initMap() {
+  // Coords of Glowing Grates
+  const position = { lat: 51.01569966994804, lng: -3.0926557886122468 };
+  // Request needed libraries.
+  //@ts-ignore
+  const { Map } = await google.maps.importLibrary("maps");
+  const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+
+  // Map position, Zoom Level
+  map = new Map(document.getElementById("map"), {
+    zoom: 17,
+    center: position,
+    mapId: "GlowingGratesMAP",
+  });
+
+  //Marker positioned at Glowing Grates location
+  const marker = new AdvancedMarkerElement({
+    map: map,
+    position: position,
+    title: "Glowing Grates",
+  });
+}
+
+initMap();
+
+ 
+
+
+
+//Darkmode - Accessiblity Features
